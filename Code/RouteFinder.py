52fe9084f39a0cecff4432d8f2070eb25ed9030c
cauprkprj <comnamu18@gmail.com>
2018-10-01 10:40:12 +0900

    5th Week Complete
    ToDo : Error correcting at RouteFinder.py

--- Code/RouteFinder.py
+++ Code/RouteFinder.py
@@ -1,39 +1,26 @@
 # -*- coding: utf-8 -*-
-# Currently, this code is for API which return JSON type
-# TODO : Change JSON type to XML / Save DB at another path
-
 
 import requests
-import sqlite3
-from xml.etree.ElementTree import parse
-from pandas import Series, DataFrame
+import pandas as pd
+import urllib
+import json
 
 def requestData() :
     APP_KEY = 'g%2F5KztLAmoWSiTeIdbG0jZXlwG4YGfheb0P7zQzKoxZBapGCIMWPIkMuQRQ9nB1YXMmFBXJi6fKxBJshDQmxZA%3D%3D'
-    Operation = 'getCtyCodeList'
-    ret = DataFrame()
-    hosturl = 'http://openapi.tago.go.kr/openapi/service/BusRouteInfoInqireService/getCtyCodeList'
-    hosturl = hosturl + Operation + '?ServiceKey=' + APP_KEY
+    OPName = 'getCtyCodeList?'
+    hosturl = 'http://openapi.tago.go.kr/openapi/service/BusRouteInfoInqireService/'
+    f = {urllib.parse.quote_plus('ServiceKey') : APP_KEY, urllib.parse.quote_plus('파라미터영문명'):'파라미터기본값'}
+    hosturl = hosturl + OPName + urllib.parse.urlencode(f)
+    print(hosturl)
     myResponse = requests.get(hosturl)
+    ret = pd.DataFrame()
     if(myResponse.ok):
-        # Loading the response data into a dict variable
-        # json.loads takes in only binary or string variables so using content to fetch binary content
-        # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)        ret = DataFrame(jData)
-        tree = parse(myResponse)
-        note = tree.getroot()
-        print(ret)
-
-        #ret = ret.append(output, ignore_index = True)
+        jData = json.loads(myResponse.content)
+        print(jData)
     else:
-        # If response code is not ok (200), print the resulting http error code with description
-            myResponse.raise_for_status()
+        print("WRONG!")
+        myResponse.raise_for_status()
+
     return ret
 
-source = requestData()
-# SQLite DB 연결
-conn = sqlite3.connect("test.db")
-# Connection 으로부터 Cursor 생성
-cur = conn.cursor()
-source.to_sql("station", conn)
-# Connection 닫기
-conn.close()
+requestData()
--- Code/create_node.py
+++ Code/create_node.py
@@ -1,2 +1,20 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
 
+import pandas as pd
+import numpy as np
+
+fileName = 'NodeData.csv'
+rawData = pd.read_csv(fileName)
+
+#return bus stop's nummber
+def createNode() :
+    return pd.Series(rawData['정류소번호'])
+#return bus stop's info by station number
+def busStopInfo(busstop) :
+    ret = list(np.where(rawData['정류소번호'] == busstop)[0])
+    return rawData.iloc[ret]
+
+#Just For Testing
+print(rawData)
+print(createNode())
+print(busStopInfo(25760))
