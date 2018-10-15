a086b202ec7e1f2881cdbb39157e91406848577a
Kim SangHeon <comnamu18@gmail.com>
2018-10-15 10:14:10 +0900

    7TH WEEK
    7TH WEEK V0.7 ADDED
    WED IS FINAL DUE

--- Code/DSPRJ.py
+++ Code/DSPRJ.py
@@ -3,18 +3,21 @@ import GraphEdge as ge
 import GraphTotal as gt
 import csv
 import networkx as nx
+from networkx.algorithms.connectivity import local_node_connectivity
 import matplotlib.pyplot as plt
+import itertools
+
 #중간지점 리턴
-def getMediumPoint(path):
+def getMediumPoint(G, path):
     pathIter = iter(path)
     point = next(pathIter)
     weight = list()
     total = 0
     for i in pathIter:
-        total = total + graph.G[point][i]['weight']
+        total = total + G[point][i]['weight']
         weight.append(total)
         point = i
-    goal = int(getPathWeight(path) / 2)
+    goal = int(getPathWeight(G, path) / 2)
     goalI = 0
     for i in range(weight):
         if weight[i] > goal:
@@ -26,34 +29,41 @@ def getMediumPoint(path):
         return path[goalI - 1]
     return path[goalI]
 #경로의 weight값 리턴
-def getPathWeight(path):
+def getPathWeight(G, path):
     ret = 0
     pathIter = iter(path)
     point = next(pathIter)
     for i in pathIter:
-        ret = ret + graph.G[point][i]['weight']
+        ret = ret + G[point][i]['weight']
     return ret
 #3점에서의 약속장소 리턴
-def ThreePointShortestPath(pointList):
+def ThreePointShortestPath(G, pointList):
     mediumList = list()
-    mediumList.append(nx.dijkstra_path(graph.G, pointList[1], pointList[2], weight='weight'))
-    mediumList.append(nx.dijkstra_path(graph.G, pointList[0], pointList[2], weight='weight'))
-    mediumList.append(nx.dijkstra_path(graph.G, pointList[0], pointList[1], weight='weight'))
+    mediumList.append(nx.dijkstra_path(G, pointList[1], pointList[2], weight='weight'))
+    mediumList.append(nx.dijkstra_path(G, pointList[0], pointList[2], weight='weight'))
+    mediumList.append(nx.dijkstra_path(G, pointList[0], pointList[1], weight='weight'))
     mediumPoint = list()
     for i in mediumList:
-        mediumPoint.append(getMediumPoint(i))
-    
+        mediumPoint.append(getMediumPoint(G, i))
     point = -1
     weight = - 1
     for i in range(3):
-        tmp = nx.dijkstra_path_length(graph.G, mediumPoint[i], pointList[i], weight='weight')
-        tmp = tmp + getPathWeight(mediumList[i])
+        tmp = nx.dijkstra_path_length(G, mediumPoint[i], pointList[i], weight='weight')
+        tmp = tmp + getPathWeight(G, mediumList[i])
         if weight == -1 or weight > tmp:
             weight = tmp
             point = i
 
-    return getMediumPoint(nx.dijkstra_path(graph.G, mediumPoint[point], pointList[point], weight = 'weight'))
-
+    return getMediumPoint(G, nx.dijkstra_path(graph.G, mediumPoint[point], pointList[point], weight = 'weight'))
+def chcekConnectivity(G, startNode):
+    ret = None
+    for u, v in itertools.combinations(startNode, 2):
+        k = local_node_connectivity(G, u, v)
+        if k == 0 :
+            ret = False
+    if ret is None:
+        ret = True
+    return ret
 
 SERVICE_KEY = 'serviceKey=5CQ9SA5qWhO0FwqcEFZDa%2BBreOSi1VBRXxQcGgpQQw%2FghXivHoiMgMRzzblvToQ00GoZuqIStC%2Ft0zxZiyyYlw%3D%3D&'
 APP_KEY = input("Plese type Admin Key : ")
@@ -75,14 +85,13 @@ for i in range(TotalUser):
     selectedItem = example[int(selectNum) - 1][1]
     startNode.append(int(selectedItem))
 
-if nx.all_pairs_node_connectivity(graph.G, startNode) is None:
+if not chcekConnectivity(graph.G, startNode):
     print("Nodes are not connected!")
 else:
     ret = list()
     if len(startNode) == 2:
         ret.append(nx.dijkstra_path(graph.G, startNode[0], startNode[1], weight='weight'))
     else:
-        #이부분 다시..
         threePointList = list()
         for i in startNode:
             tmpList = list(startNode)
@@ -95,10 +104,9 @@ else:
                     threePointList.append(item)
         ret = -1
         way = list()
-        for i in threePointList:
-            cal = ThreePointShortestPath([i[0], i[1], i[2]])
-            if ret == -1 or cal < ret:
-                cal = ret
-                way = list(i)
+        cal = ThreePointShortestPath(graph.G, [i[0], i[1], i[2]])
+        if ret == -1 or cal < ret:
+            cal = ret
+            way = list(i)
 
 graph.drawGraph()
